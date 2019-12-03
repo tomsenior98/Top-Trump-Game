cf4c13ddc3cee05979ccd9cbbdc3ef57719447a5
tomsenior20 <tomsenior>
2019-12-03 16:25:06 +0000

    refactored the js script

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
+const image = document.getElementById("image1");
 function randomNumber1() {
-    var random = Math.ceil(Math.random() * 6)
-    // console.log(random);
-    const image = document.getElementById("image1");
-    if (random == 1) {
-        image.src = "./images/messi.jpg";
-    }
-    if (random == 2) {
-        image.src = "./images/wright.jpeg";
-    }
-    if (random == 3) {
-       image.src = "./images/ramos.jpg";
-    }
-    if (random == 4) {
-       image.src = "./images/vieira.jpg";
-    }
-    if (random == 5) {
-        image.src = "./images/edu.jpeg";
-    }
-    if (random == 6) {
-       image.src = "./images/theirry.jpg";
-    }
+    var random = Math.ceil(Math.random() * 12)
+     image.src=`./images/${random}.png`;
 }
 randomNumber1();
 
+const images = document.getElementById("image2");
 function randomNumber2() {
-    var random = Math.ceil(Math.random() * 6)
-    // console.log(random);
-    const image = document.getElementById("image2");
-    if (random == 1) {
-        image.src = "./images/rooney.jpg";
-    }
-    if (random == 2) {
-        image.src = "./images/pogba.jpg";
-    }
-    if (random == 3) {
-       image.src = "./images/benteke.jpg";
-    }
-    if (random == 4) {
-       image.src = "./images/beckham.jpg";
-    }
-    if (random == 5) {
-        image.src = "./images/zlatan.jpg";
-    }
-    if (random == 6) {
-       image.src = "./images/zidane.jpg";
-    }
+    var randoms = Math.ceil(Math.random() * 12)
+     image2.src= `./images/${randoms}.png`;
 }
 randomNumber2();
+
+// function randomNumber2() {
+//     var random = Math.ceil(Math.random() * 12);
+//     // console.log(random);
+//     const image = document.getElementById("image2");
+//     // if (random == 1) {
+//     //     image.src = "./images/rooney.jpg";
+//     // }
+//     // if (random == 2) {
+//     //     image.src = "./images/pogba.jpg";
+//     // }
+//     // if (random == 3) {
+//     //    image.src = "./images/benteke.jpg";
+//     // }
+//     // if (random == 4) {
+//     //    image.src = "./images/beckham.jpg";
+//     // }
+//     // if (random == 5) {
+//     //     image.src = "./images/zlatan.jpg";
+//     // }
+//     // if (random == 6) {
+//     //    image.src = "./images/zidane.jpg";
+//     // }
+//     image.src=`./images/${random}.png`;
+//     console.log(image)
+
+// }
+// randomNumber2();
 
