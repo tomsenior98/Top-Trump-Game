5f1838920ca96644cf6cbd49a4930e0e79889966
tomsenior20 <tomsenior>
2019-12-03 14:13:26 +0000

    second card stack images changed

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function randomNumber1() {
     var random = Math.ceil(Math.random() * 6)
-    console.log(random);
+    // console.log(random);
     const image = document.getElementById("image1");
-    // const image = document.getElementById("image2");
-
     if (random == 1) {
         image.src = "./images/messi.jpg";
     }
@@ -27,28 +25,26 @@ randomNumber1();
 
 function randomNumber2() {
     var random = Math.ceil(Math.random() * 6)
-    console.log(random);
+    // console.log(random);
     const image = document.getElementById("image2");
     if (random == 1) {
-        image.src = "./images/messi.jpg";
+        image.src = "./images/rooney.jpg";
     }
     if (random == 2) {
-        image.src = "./images/wright.jpeg";
+        image.src = "./images/pogba.jpg";
     }
     if (random == 3) {
-       image.src = "./images/ramos.jpg";
+       image.src = "./images/benteke.jpg";
     }
     if (random == 4) {
-       image.src = "./images/vieira.jpg";
+       image.src = "./images/beckham.jpg";
     }
     if (random == 5) {
-        image.src = "./images/edu.jpeg";
+        image.src = "./images/zlatan.jpg";
     }
     if (random == 6) {
-       image.src = "./images/theirry.jpg";
+       image.src = "./images/zidane.jpg";
     }
 }
 randomNumber2();
-
-
 
