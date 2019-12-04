474f5f6f4bf216f318e7504b09f654a6e83ac8c2
tomsenior20 <tomsenior>
2019-12-04 09:44:10 +0000

    Deleted previous useless code and added comments

--- index.js
+++ index.js
@@ -11,32 +11,3 @@ function randomNumber2() {
      image2.src= `./images/${randoms}.png`;
 }
 randomNumber2();
-
-// function randomNumber2() {
-//     var random = Math.ceil(Math.random() * 12);
-//     // console.log(random);
-//     const image = document.getElementById("image2");
-//     // if (random == 1) {
-//     //     image.src = "./images/rooney.jpg";
-//     // }
-//     // if (random == 2) {
-//     //     image.src = "./images/pogba.jpg";
-//     // }
-//     // if (random == 3) {
-//     //    image.src = "./images/benteke.jpg";
-//     // }
-//     // if (random == 4) {
-//     //    image.src = "./images/beckham.jpg";
-//     // }
-//     // if (random == 5) {
-//     //     image.src = "./images/zlatan.jpg";
-//     // }
-//     // if (random == 6) {
-//     //    image.src = "./images/zidane.jpg";
-//     // }
-//     image.src=`./images/${random}.png`;
-//     console.log(image)
-
-// }
-// randomNumber2();
-
